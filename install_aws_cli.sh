0ddd01520c59ced588bd3b7885aa06d8d3c110b1
phani-rudra9 <53288879+phani-rudra9@users.noreply.github.com>
2021-01-18 01:12:15 +0530

    Update install_aws_cli.sh

--- install_aws_cli.sh
+++ install_aws_cli.sh
@@ -7,15 +7,3 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 unzip awscliv2.zip
 
 sudo ./aws/install
-
-aws
-
-aws configure
-
-AWS Access Key ID [None]: aa
-
-AWS Secret Access Key [None]: bb
-
-Default region name [None]: cc
-
-Default output format [None]: json
