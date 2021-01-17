f123c27dd8cdd966ce8321f5bdf1bebbdb5d5409
phani-rudra9 <53288879+phani-rudra9@users.noreply.github.com>
2021-01-18 00:10:03 +0530

    Update install_aws_cli.sh

--- install_aws_cli.sh
+++ install_aws_cli.sh
@@ -12,12 +12,3 @@ sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
 
 ./awscli-bundle/install -h
 
-aws configure
-
-AWS Access Key ID [None]: $2
-
-AWS Secret Access Key [None]: $3 
-
-Default region name [None]: $4
-
-Default output format [None]: json
