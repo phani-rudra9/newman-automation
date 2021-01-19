7833eff0b54dca565df9f0359f4c5df4102243a9
phanindra@monocept.com-at-971076122335 <phanindra@monocept.com-at-971076122335>
2021-01-20 00:19:04 +0530

    terraform commit

---
+++ install_terraform.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt-get update -y 
+wget https://releases.hashicorp.com/terraform/0.14.2/terraform_0.14.2_linux_amd64.zip
+sudo apt install unzip
+unzip terraform_0.14.2_linux_amd64.zip
+sudo mv terraform_0.14.2_linux_amd64/terraform /usr/bin
